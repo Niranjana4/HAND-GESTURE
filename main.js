3f98e4a362ffd44379ebcc293dd7324cf1bc11b9
Niranjana4 <75379681+Niranjana4@users.noreply.github.com>
2020-12-12 18:50:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,51 @@ function speak()
     var utterthis= new SpeechSynthesisUtterance(speakdata1+speakdata2);
     synth.speak(utterthis);
 }
+
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,result)
+{
+    if(error)
+    {
+        console.error(error)
+    }
+      else{
+          console.log(result)
+          document.getElementById("result_emotion_name").innerHTML=result[0].label;
+          document.getElementById("result_emotion_name2").innerHTML=result[1].label;
+          prediction1=result[0].label;
+          prediction2=result[1].label;
+          speak();
+          if{result[0].label=="Happy"}
+          {
+              document.getElementById("update_emoji").innerHTML="&#128522";
+          }
+          if{result[0].label=="Sad"}
+          {
+              document.getElementById("update_emoji").innerHTML="&#128546";
+          }
+          if{result[0].label=="Angry"}
+          {
+              document.getElementById("update_emoji").innerHTML="&#128545";
+          }
+          if{result[1].label=="Happy"}
+          {
+              document.getElementById("update_emoji2").innerHTML="&#128522";
+          }
+          if{result[1].label=="Sad"}
+          {
+              document.getElementById("update_emoji2").innerHTML="&#128546";
+          }
+          if{result[1].label=="Angry"}
+          {
+              document.getElementById("update_emoji2").innerHTML="&#128545";
+          }
+      }
+}
+
